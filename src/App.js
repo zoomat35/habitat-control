a156285f00b3e97cc8450fc2e789d16cd7b70c11
zoomat35 <zoomat35@gmail.com>
2025-09-09 01:48:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Panel de Control de Hábitats</h1>
-      <HabitatCard habitatId={1} />
-      <HabitatCard habitatId={2} />
+      <HabitatCard habitatId={1} releId={1} />
+      <HabitatCard habitatId={2} releId={2} />
     </div>
   );
 }
