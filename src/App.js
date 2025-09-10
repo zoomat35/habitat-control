fb4ccee99c424bfb071e7ad768632b18458308b8
zoomat35 <zoomat35@gmail.com>
2025-09-09 22:04:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HabitatCard from './HabitatCard';
+import ReleBoton from './ReleBoton';
 
 function App() {
   return (
@@ -6,12 +6,12 @@ function App() {
       <h1>Panel de Control de Hábitats</h1>
 
       <h2>Hábitat 1</h2>
-      <HabitatCard habitatId={1} releId={1} />
-      <HabitatCard habitatId={1} releId={2} />
+      <ReleBoton habitatId={1} releId={1} />
+      <ReleBoton habitatId={1} releId={2} />
 
       <h2>Hábitat 2</h2>
-      <HabitatCard habitatId={2} releId={1} />
-      <HabitatCard habitatId={2} releId={2} />
+      <ReleBoton habitatId={2} releId={1} />
+      <ReleBoton habitatId={2} releId={2} />
     </div>
   );
 }
