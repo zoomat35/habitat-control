6c2ecd429741b99feeedaf0857cb21410db6caaa
zoomat35 <zoomat35@gmail.com>
2025-09-09 22:45:42 -0600

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://gehwsxrjtgjecjikhhut.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlaHdzeHJqdGdqZWNqaWtoaHV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczODA5MzEsImV4cCI6MjA3Mjk1NjkzMX0.0sm4L2l3r8vsrg8S8Xy85ugwrTS5ilBIj_pZoLLeGVY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
