61906b4cab5846fdb4026547622608088c0a82da
zoomat35 <zoomat35@gmail.com>
2025-09-09 21:56:09 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ function App() {
     <div style={{ padding: '2rem' }}>
       <h1>Panel de Control de Hábitats</h1>
 
-      {/* Hábitat 1 */}
+      <h2>Hábitat 1</h2>
       <HabitatCard habitatId={1} releId={1} />
       <HabitatCard habitatId={1} releId={2} />
-      <HabitatCard habitatId={1} releId={3} />
 
-      {/* Hábitat 2 */}
+      <h2>Hábitat 2</h2>
       <HabitatCard habitatId={2} releId={1} />
       <HabitatCard habitatId={2} releId={2} />
-      <HabitatCard habitatId={2} releId={3} />
     </div>
   );
 }
